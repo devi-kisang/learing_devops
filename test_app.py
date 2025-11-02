82e9bd39824a263dc65e5be9708bf46a9c3c4406
devi-kisang <devi.kisang@gmail.com>
2025-11-02 12:47:25 +0530

    ci/cd pipeline

--- test_app.py
+++ test_app.py
@@ -7,10 +7,9 @@ def test_root_endpoint():
     """Tests the main "/" endpoint."""
     response = client.get("/")
     
-    # Check that the request was successful
+  
     assert response.status_code == 200
-    
-    # Check that the response data is exactly what we expect
+
     assert response.json() == {
         "message": "Hello, DEVOPS!!!!!"
     }
